ba9e860275b91a8dc30b498d9a1d97cf2a860563
seifajax04 <saifberlin04@gmail.com>
2024-04-08 02:49:41 +0200

    Password checker App

---
+++ Exams/Password Checker App.py
@@ -0,0 +1,7 @@
+#Password checker App
+
+username = input('Enter your username:\t')
+password = input('Enter you password:\t')
+
+secret_password = len(password) * '*'
+print(f'Hey {username}, your password {secret_password} is {len(password)} letters long.')
